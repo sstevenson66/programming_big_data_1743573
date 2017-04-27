f621a5fd99dd4f3dd8d67ca8ea1efdaab454b169
sstevenson66 <1743573@mydbs.ie>
2017-04-27 17:36:24 +0100

    Further changes, added day of week and functions to return counts

--- change_log.py
+++ change_log.py
@@ -9,11 +9,12 @@ class CommitDetail(object):
 		
 		
 class CommitHeader(object):
-	def __init__(self, revision = None, author = None, date = None, commentLineCount = None, 
+	def __init__(self, revision = None, author = None, date = None, commentLineCount = None, day = None,
 			comment = None):
 		self.Revision = revision
 		self.Author = author
 		self.DateTime = date
+		self.DayOfWeek = day
 		self.CommentLineCount = commentLineCount
 		self.Changes = []
 		self.Comment = comment
@@ -24,7 +25,25 @@ def load_data(filename):
 	data = [line.strip() for line in open(filename, "r")]
 	return data
 
-	
+def return_no_of_commits(data, author = None):
+    found = 0
+    if author == None:
+        return len(data)
+    for idx in range(0, len(data) - 1):
+        if author in data[idx].Author:
+            found = found + 1
+    return found
+    
+def return_no_of_change_types(data, change_type, author = None):
+	idx_change = 0
+	found = 0
+	for idx in range(0, len(data) - 1):
+		for idx_change in range(0, len(data[idx].Changes) - 1):
+			if data[idx].Changes[idx_change].change_type == change_type:
+				if author == None or author in data[idx].Author:
+					found = found + 1
+	return found
+    
 def CreateMatrix(data):
 	sep = 72 * '-'
 	idx = 0
@@ -32,27 +51,38 @@ def CreateMatrix(data):
 
 	while idx < len(data) - 1:
 		if data[idx] == sep:
+			#
+			#	If Separator then next line is header - Fill in HeaderDetails
+			#
 			HeaderDetails = data[idx + 1].split('|')
 			NewCommitHeader = CommitHeader(HeaderDetails[0], HeaderDetails[1], HeaderDetails[2], HeaderDetails[3])
-			for idx in range(idx+3,data.index('', idx + 3)):
+			i1 = (NewCommitHeader.DateTime.find("(") + 1)
+			i2 =  NewCommitHeader.DateTime.find(",", (NewCommitHeader.DateTime.find("(") + 1))
+			NewCommitHeader.DayOfWeek = (NewCommitHeader.DateTime[i1:i2])
 			#
 			#	Loop through path changes and add into CommitChange object
 			#
+			for idx in range(idx+3,data.index('', idx + 3)):
 				CommitDetails = data[idx].split(' ')
 				NewCommitDetail = CommitDetail(CommitDetails[0], CommitDetails[1])
 				NewCommitHeader.Changes.append(NewCommitDetail)
 				NewCommitHeader.NoOfChanges =	NewCommitHeader.NoOfChanges + 1
-			#print len(HeaderDetails)
 			NewCommitFile.append(NewCommitHeader)
 			print "idx = ", idx, "next is ", data.index(sep, idx + 1), " NewCommitFile length = ", len(NewCommitFile)
 		idx = idx + 1
 	return NewCommitFile
 
-
 	
 if __name__ == "__main__":
 	data = load_data("changes_python.log")
 	FullData = CreateMatrix(data)
-	print len(FullData)
+	print(return_no_of_commits(FullData))
+	print(return_no_of_commits(FullData, "Thomas"))
+	print(return_no_of_change_types(FullData, "D"))
+	print(return_no_of_change_types(FullData, "A"))
+	print(return_no_of_change_types(FullData, "M"))
+	print(return_no_of_change_types(FullData, "D", "Thomas"))
+	
+
 
 
