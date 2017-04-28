25f48641eda58d4bb77cc2a76715ae3011f07e94
Seb Stevenson <Seb Stevenson>
2017-04-28 21:41:06 +0100

    Further changes to test script for changes_python.log

--- test_change.py
+++ test_change.py
@@ -16,6 +16,14 @@ class TestCommits(unittest.TestCase):
         self.assertEqual(743,return_no_of_change_types(self.NewCommitFile, 'D'))
         self.assertEqual(646,return_no_of_change_types(self.NewCommitFile, 'D', 'Thomas'))
         self.assertEqual(31,return_no_of_change_types(self.NewCommitFile, 'D', 'Vincent'))
+        
+    def test_number_of_commits(self):
+        self.assertEqual(422, return_no_of_commits(self.NewCommitFile))
+        self.assertEqual(26, return_no_of_commits(self.NewCommitFile, "Vincent"))
+        self.assertEqual(190, return_no_of_commits(self.NewCommitFile, "Thomas"))
+        self.assertEqual(152, return_no_of_commits(self.NewCommitFile, "Jimmy"))
+        self.assertEqual(5, return_no_of_commits(self.NewCommitFile, "Alan"))
+        
 
 if __name__ == '__main__':
     unittest.main()
