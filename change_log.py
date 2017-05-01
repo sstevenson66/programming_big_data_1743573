c6d718bb8dc32295419e9e4dea9300118575455c
sstevenson66 <1743573@mydbs.ie>
2017-05-01 18:31:22 +0100

    Added list by day

--- change_log.py
+++ change_log.py
@@ -1,4 +1,4 @@
-#  Commit has Revison Number, Name, Date etc
+import os
 #
 class CommitDetail(object):
 	def __init__(self, change_type, change_line):
@@ -32,12 +32,12 @@ def return_no_of_commits(data, author = None):
             found = found + 1
     return found
     
-def return_no_of_change_types(data, change_type, author = None):
+def return_no_of_change_types(data, change_type = None, author = None):
 	idx_change = 0
 	found = 0
-	for idx in range(0, len(data) - 1):
-		for idx_change in range(0, len(data[idx].Changes) - 1):
-			if data[idx].Changes[idx_change].change_type == change_type:
+	for idx in range(0, len(data)):
+		for idx_change in range(0, len(data[idx].Changes)):
+			if data[idx].Changes[idx_change].change_type == change_type or change_type == None:
 				if author == None or author in data[idx].Author:
 					found = found + 1
 	return found
@@ -54,7 +54,6 @@ def CreateMatrix(data):
 			#
 			HeaderDetails = data[idx + 1].split('|')
 			NewCommitHeader = CommitHeader(HeaderDetails[0], HeaderDetails[1], HeaderDetails[2], HeaderDetails[3])
-                        print(NewCommitHeader.Author)
 			i1 = (NewCommitHeader.DateTime.find("(") + 1)
 			i2 =  NewCommitHeader.DateTime.find(",", (NewCommitHeader.DateTime.find("(") + 1))
 			NewCommitHeader.DayOfWeek = (NewCommitHeader.DateTime[i1:i2])
@@ -71,28 +70,61 @@ def CreateMatrix(data):
 		idx = idx + 1
 	return NewCommitFile
 
-	
-if __name__ == "__main__":
-    data = load_data("changes_python.log")
-    FullData = CreateMatrix(data)
-    print(return_no_of_commits(FullData, "Vincent"))
-    
-    print FullData[0].Author, " 0"
+def display_results_by_author():
     FullData.sort(key = lambda i: (i.Author).lower())
-    print(return_no_of_commits(FullData))
-    print(" Author                                               Commits   Del   Add   Mod")
-    print("--------------------------------------------------    -------   ---   ---   ---")
+    print(" Author                                               Commits   Del   Add   Mod  Read   ` Total")
+    print("--------------------------------------------------    -------   ---   ---   ---  ----     -----")
     prev_Author = " "
+    
     for f in FullData:
         if f.Author <> prev_Author:
-            print("{:50} {:10} {:5} {:5} {:5}").format(f.Author, return_no_of_commits(FullData, f.Author), return_no_of_change_types(FullData, "D", f.Author), return_no_of_change_types(FullData, "A", f.Author), return_no_of_change_types(FullData, "M", f.Author))
+            print("{:50} {:10} {:5} {:5} {:5} {:5} {:9}").format(f.Author, return_no_of_commits(FullData, f.Author), return_no_of_change_types(FullData, "D", f.Author), return_no_of_change_types(FullData, "A", f.Author), return_no_of_change_types(FullData, "M", f.Author), return_no_of_change_types(FullData, "A", f.Author), return_no_of_change_types(FullData, None, f.Author))
             prev_Author = f.Author
-    print "____"
-    #print(return_no_of_commits(FullData))
-    #print(return_no_of_commits(FullData, "Thomas"))
-    print(return_no_of_change_types(FullData, "D"))
-    print(return_no_of_change_types(FullData, "A"))
-    print(return_no_of_change_types(FullData, "M"))
-    print(return_no_of_change_types(FullData, "D", "Thomas"))
+    print("--------------------------------------------------    -------   ---   ---   ---  ----     -----")
+    print("{:50} {:10} {:5} {:5} {:5} {:5} {:9}").format("Total", return_no_of_commits(FullData), return_no_of_change_types(FullData, "D"), return_no_of_change_types(FullData, "A"), return_no_of_change_types(FullData, "M"), return_no_of_change_types(FullData, "R"), return_no_of_change_types(FullData))
+
+def display_results_by_day():
+    FullData.sort(key = lambda i: (i.DayOfWeek).lower())
+    print(" Day                                                  Commits   Del   Add   Mod  Read     Total")
+    print("--------------------------------------------------    -------   ---   ---   ---  ----     -----")
+    prev_DayOfWeek = None
+    total_commits = 0
+    total_del = 0
+    total_add = 0
+    total_read = 0
+    total_mod = 0
+    
+    for f in FullData:
+        if f.DayOfWeek <> prev_DayOfWeek:
+            if prev_DayOfWeek <> None:
+                print("{:50} {:10} {:5} {:5} {:5} {:5} {:9}").format(prev_DayOfWeek, total_commits, total_del, total_add, total_mod, total_read, (total_del + total_add + total_mod + total_read))
+                total_commits = 0
+                total_add = 0
+                total_del = 0
+                total_read = 0
+                total_mod = 0
+            prev_DayOfWeek = f.DayOfWeek
+        total_commits = total_commits + 1
+        for g in f.Changes:
+            if g.change_type == "D":
+                total_del = total_del + 1
+            elif g.change_type == "M":
+                total_mod = total_mod + 1
+            elif g.change_type == "A":
+                total_add = total_add + 1
+            elif g.change_type == "R":
+                total_read = total_read + 1
+            else:
+                print "No such change type catered for - ", g.change_type
+    print("--------------------------------------------------    -------   ---   ---   ---  ----     -----")
+    print("{:50} {:10} {:5} {:5} {:5} {:5} {:9}").format("Total", return_no_of_commits(FullData), return_no_of_change_types(FullData, "D"), return_no_of_change_types(FullData, "A"), return_no_of_change_types(FullData, "M"), return_no_of_change_types(FullData, "R"), return_no_of_change_types(FullData))
+
 	
+if __name__ == "__main__":
+    os.system('cls')
+    data = load_data("changes_python.log")
+    FullData = CreateMatrix(data)
+    display_results_by_author()
+    print("\n\n")
+    display_results_by_day()
     
